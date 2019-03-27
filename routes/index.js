20ef5d2ef288af1554d9f11ffa9c8b01a6c49fa8
Blake Burroughs <b.burro@gmail.com>
2019-03-27 14:37:09 -0500

    working on errors and routes

--- routes/index.js
+++ routes/index.js
@@ -13,9 +13,18 @@ router.get('/about', (req, res) => {
 });
 
 router.get('/project/:id', (req, res) => {
-    const { id } = data.projects;
+    res.locals.projects = data.projects;
+    const { id } = req.params;
+    const name = res.locals.projects[id].project_name;
+    const description = res.locals.projects[id].description;
+    const technologies = res.locals.projects[id].technologies;
+    const liveLink = res.locals.projects[id].live_link;
+    const gitRepo = res.locals.projects[id].github_link;
+    const images = res.locals.projects[id].image_urls;
     
-    res.render('project'); 
+    const templateData = { id, name, description, technologies, liveLink, gitRepo, images };
+    
+    res.render('project', { templateData }); 
 });
 
 
